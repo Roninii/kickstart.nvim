c1059be9b9e691fc46909fefe124435d5a6821b7
Ronnie Villarini <ronnie@ronini.dev>
2024-03-24 15:31:52 -0500

    add mini.cursorword for hovered work highlights

--- init.lua
+++ init.lua
@@ -726,6 +726,9 @@ require('lazy').setup({
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
+
+      -- Automatic highlighting of word under cursor
+      require('mini.cursorword').setup()
     end,
   },
   { -- Highlight, edit, and navigate code
